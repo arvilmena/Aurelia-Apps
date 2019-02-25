6065fa8c4726c10ac241e7a71f6968ad60ba31d1
Arvil Meña <arvilmena@gmail.com>
2019-02-25 10:13:28 +0800

    implement the routing system defined in the tutorial

--- contact-manager/aurelia-app/src/app.ts
+++ contact-manager/aurelia-app/src/app.ts
@@ -1,5 +1,23 @@
-import {Aurelia} from 'aurelia-framework';
+import {Router, RouterConfiguration} from 'aurelia-router';
 import {PLATFORM} from 'aurelia-pal';
 export class App {
-  public message: string = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'Contacts';
+    config.map([
+      {
+        route: '',
+        moduleId: PLATFORM.moduleName('no-selection'),
+        title: 'Select'
+      },
+      {
+        route: 'contacts/:id',
+        moduleId: PLATFORM.moduleName('contact-detail'),
+        name: 'contacts', 
+      }
+    ]);
+
+    this.router = router;
+  }
 }
