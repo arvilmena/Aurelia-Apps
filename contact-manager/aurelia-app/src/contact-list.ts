42dbed84a0336267a2d90c95b0e4a22f1a8150a8
Arvil Mena <arvilmena@gmail.com>
2019-02-25 13:24:40 +0000

    implementing the eventAggregator to contact-list module

--- contact-manager/aurelia-app/src/contact-list.ts
+++ contact-manager/aurelia-app/src/contact-list.ts
@@ -1,12 +1,21 @@
 import { WebAPI } from './web-api';
 import { inject } from 'aurelia-framework';
+import { ContactViewed, ContactUpdated } from './message';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
-@inject(WebAPI)
+@inject(WebAPI, EventAggregator)
 export class ContactList {
   contacts;
   selectedId = 0;
 
-  constructor(private api: WebAPI) { }
+  constructor(private api: WebAPI, private ea: EventAggregator) {
+    ea.subscribe(ContactViewed, msg => this.select(msg.contact));
+    ea.subscribe(ContactUpdated, msg => {
+      let id = msg.contact.id;
+      let found = this.contacts.find(x => x.id === id);
+      Object.assign(found, msg.contact);
+    });
+  }
 
   created() {
     this.api.getContactList().then(contacts => this.contacts = contacts);
