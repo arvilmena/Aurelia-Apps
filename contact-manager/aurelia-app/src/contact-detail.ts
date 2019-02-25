015f1c4d1c3bc6092c62f13fb52a8f50bf764446
Arvil Meña <arvilmena@gmail.com>
2019-02-25 11:56:41 +0800

    contact-detail module -- backend only (before adding comments)

--- contact-manager/aurelia-app/src/contact-detail.ts
+++ contact-manager/aurelia-app/src/contact-detail.ts
@@ -1,1 +1,48 @@
-export class ContactDetail {}
+import { WebAPI } from './web-api';
+import {inject} from 'aurelia-framework';
+import {areEqual} from './utility';
+
+interface Contact {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+@inject(WebAPI)
+export class ContactDetail {
+  routeConfig;
+  contact: Contact;
+  originalContact: Contact;
+
+  constructor(private api: WebAPI) {}
+
+  activate(params, routeConfig) {
+    this.routeConfig = routeConfig;
+
+    return this.api.getContactDetails(params.id).then(contact => {
+      this.contact = <Contact>contact;
+      this.routeConfig.navModel.setTitle(this.contact.firstName);
+      this.originalContact = JSON.parse(JSON.stringify(this.contact));
+    });
+  }
+
+  get canSave() {
+    return this.contact.firstName && this.contact.lastName && !this.api.isRequesting;
+  }
+
+  save() {
+    this.api.saveContact(this.contact).then(contact => {
+      this.contact = <Contact>contact;
+      this.routeConfig.navModel.setTitle(this.contact.firstName);
+      this.originalContact = JSON.parse(JSON.stringify(this.contact));
+    });
+  }
+  
+  canDeactivate() {
+    if (!areEqual(this.originalContact, this.contact)) {
+      return confirm('You have unsaved changes. Are you sure you wish to leave?');
+    }
+
+    return true;
+  }
+}
