eb99f547d6bc5b1d34f8f586ab88eb68bce668aa
Arvil Mena <arvilmena@gmail.com>
2019-02-26 14:20:29 +0000

    adding explanations to loading-indicator.ts

--- contact-manager/aurelia-app/src/resources/elements/loading-indicator.ts
+++ contact-manager/aurelia-app/src/resources/elements/loading-indicator.ts
@@ -2,8 +2,15 @@ import { bindable, noView, PLATFORM } from 'aurelia-framework';
 import * as nprogress from 'nprogress';
 import 'nprogress/nprogress.css';
 
-@noView
+@noView // we don't need Aurelia to render this component, no loading-indicator.html
 export class LoadingIndicator {
+  // a decorator.
+  // 1. we want our HTML element to have `loading` property that we can bind to,
+  // this is done via HTML attribute in the DOM.
+  // 2. When there's `bindable` we can optionally declare a `propertyNameChanged` method
+  // that will be called whenever the binding system updates the property.
+  // 3. With loading property binded we can toggle NProgress off/on based on the value
+  // of this property.
   @bindable loading = false;
 
   loadingChanged(newValue) {
